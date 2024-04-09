b8094e776c3024c5804431ca630a223e0514a555
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-04-10 01:02:35 +0530

    add : Services Model Creation

---
+++ Backend/services/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import services
+
+class UserServiceForm(forms.ModelForm):
+    class Meta:
+        model = services
+        fields = "__all__"
--- Backend/services/models.py
+++ Backend/services/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from users.models import newUser
+from engine.settings import AUTH_USER_MODEL
 
 # Create your models here.
+
+class services(models.Model):
+    user = models.ManyToManyField(AUTH_USER_MODEL, related_name="users" ,null=True, blank = True, on_delete=models.CASCADE)
+    username = models.CharField(null=True, blank = True,default="NULL")
+    money = models.FloatField(null=True, blank = True,default=0)
+    text_to_speech = models.BooleanField(null=True, blank = True,default=False)
+    speech_to_text = models.BooleanField(null=True, blank = True,default=False)
+    speech_to_speech = models.BooleanField(null=True, blank = True,default=False)
+    
+    def __str__(self):
+        return f"{self.username} - TTS : {self.text_to_speech} , STT : {self.speech_to_text} , STS : {self.speech_to_speech}"
+    
+    
+     
+    
