e33aec3c06015aabca1542353f126b1770a50d15
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-05-02 12:57:17 +0530

    add : custom is_valid method and messagestack for error messages

--- Backend/users/serializers.py
+++ Backend/users/serializers.py
@@ -13,39 +13,54 @@ class UserModelSerializer:
     But before going to serialize , the raw data undergoes several Validation Checks.
     '''
     def __init__(self,username,email,password,first_name,last_name) -> None:
-        self.model = NewUser
         self.username = username
         self.email = email
         self.password = password
         self.first_name = first_name
         self.last_name = last_name
         self.flag = True
+        self.messagestack = []
 
         if self.username:
-            if self.model.objects.filter(username = self.username).count()==1:
+            if NewUser.objects.filter(username = self.username).count()==1:
                 self.flag = False
-                raise ValidationError(f"Username already exists")
+                self.messagestack.append({
+                    "Message":"Username already exists"   
+                })
         else:
             self.flag = False
-            raise ValidationError("Username Can't be Empty")
+            self.messagestack.append({
+                "Message":"Username Can't be Empty"   
+            })
         
         
         if self.email:
-            if self.model.objects.filter(email = self.email).count()==1:
+            if NewUser.objects.filter(email = self.email).count()==1:
                 self.flag = False
-                raise ValidationError(f"Email already exists")
+                self.messagestack.append({
+                    "Message":"Email already exists"
+                })
         else:
             self.flag = False
-            raise ValidationError("Email Can't be Empty")
+            self.messagestack.append({
+                "Message":"Email can't be Enpty"
+            })
         
         
         if self.password:
-            if self.model.objects.filter(password = self.password).count()==1:
+            if NewUser.objects.filter(password = self.password).count()==1:
                 self.flag = False
-                raise ValidationError(f"This Password is already Chosen")
+                self.messagestack.append({
+                    "Message":"Password already exists"
+                })
         else:
             self.flag = False
-            raise ValidationError("Password Can't be Empty")
+            self.messagestack.append({
+                "Message":"Password can't be empty"
+            })
+    
+    def is_valid(self) -> bool:
+        return self.flag
     
     def Get_Jwt_Tokens(self) -> dict:
         
@@ -56,19 +71,18 @@ class UserModelSerializer:
     
     def create_user_api(self):
         if self.email == os.getenv("SUPERUSER_EMAIL"):
-            userobj = self.model.objects.create_superuser(username = self.username , email = self.email , password = self.password)
+            userobj = NewUser.objects.create_superuser(username = self.username , email = self.email , password = self.password)
         else:
-            userobj = self.model.objects.create_user(username = self.username , email = self.email)
+            userobj = NewUser.objects.create_user(username = self.username , email = self.email)
         Tokens = self.Get_Jwt_Tokens()
         userobj.Bearer_Token = Tokens.get("Bearer_Token")
         userobj.Refresh_Token = Tokens.get("Refresh_Token")
-        userobj.save()
         user_api = serialize("json",userobj)
         return user_api
     
     
     def update_user_api(self,username):
-        userobj = self.model.objects.get(username = username)
+        userobj = NewUser.objects.get(username = username)
         if userobj.username != self.username:
             userobj.username = self.username
         if userobj.email != self.email:
