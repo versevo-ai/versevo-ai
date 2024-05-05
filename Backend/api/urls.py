da2e69155a9c6cb810d1c4ca4c1b25a0809560be
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-05-06 00:54:58 +0530

    add : Url for Services and Login_required Decorator

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path , include
 from users.views import *
 from services.views import *
+from django.contrib.auth.decorators import login_required
 
 # will have class based views , so will put classnames.as_view() instead of include
 
 urlpatterns = [
     path('users/',userViews.as_view()),
-    # path('services/',include('services.urls')),
+    path('users/password',login_required(PasswordView.as_view())),
+    path('services/',login_required(serviceViews.as_view())),
 ]
