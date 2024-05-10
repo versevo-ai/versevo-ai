0db3532c561b7eb87f136ef730613d22de40f076
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-05-10 11:25:46 +0530

    add : Logic for JWT Tokens Using HS256 (symmetric Cypto Algo) and changes in Update_User_Api

--- Backend/users/serializers.py
+++ Backend/users/serializers.py
@@ -1,8 +1,12 @@
+import datetime
 from django.core.serializers import serialize,deserialize
 from django.core.exceptions import ValidationError
 from .models import NewUser
 from dotenv import load_dotenv
 import os
+import jwt
+import uuid
+import random
 
 load_dotenv()
 
@@ -65,8 +69,21 @@ class UserModelSerializer:
     def Get_Jwt_Tokens(self) -> dict:
         
         # Some Code that will generate Bearer and Refresh Token Here
-        
-        Tokens=dict()
+        secret_key = "".join([random.randint(a=1,b=10) for _ in range(10)]) + "".join([str(uuid.uuid5) for _ in range(50)])
+        payload = {
+            "username":self.username,
+            "email":self.email,
+            "password":self.password
+        }
+        payload["exp"] = datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=15)
+        access_token = jwt.encode(payload=payload,key=secret_key,algorithm=["HS256"])
+        payload["exp"] = datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=40)
+        refresh_token = jwt.encode(payload=payload,key=secret_key,algorithm=["HS256"])
+        Tokens={
+            "Access_Token":access_token,
+            "Refresh_Token":refresh_token,
+            "Key":secret_key
+        }
         return Tokens
     
     def create_user_api(self):
@@ -75,25 +92,33 @@ class UserModelSerializer:
         else:
             userobj = NewUser.objects.create_user(username = self.username , email = self.email)
         Tokens = self.Get_Jwt_Tokens()
-        userobj.Bearer_Token = Tokens.get("Bearer_Token")
+        userobj.Access_Token = Tokens.get("Access_Token")
         userobj.Refresh_Token = Tokens.get("Refresh_Token")
+        userobj.Key = Tokens.get("Key")
         user_api = serialize("json",userobj)
         return user_api
     
     
     def update_user_api(self,username):
-        userobj = NewUser.objects.get(username = username)
-        if userobj.username != self.username:
-            userobj.username = self.username
-        if userobj.email != self.email:
-            userobj.email = self.email
-        if userobj.first_name != self.first_name:
-            userobj.first_name = self.first_name
-        if userobj.last_name != self.last_name:
-            userobj.last_name = self.last_name
-        userobj.save()
-        user_api = serialize("json",userobj) 
-        return user_api
+        if NewUser.objects.get(username=username).Blacklisted == False:
+            userobj = NewUser.objects.get(username = username)
+            if userobj.username != self.username:
+                userobj.username = self.username
+            if userobj.email != self.email:
+                userobj.email = self.email
+            if userobj.first_name != self.first_name:
+                userobj.first_name = self.first_name
+            if userobj.last_name != self.last_name:
+                userobj.last_name = self.last_name
+            userobj.save()
+            user_api = serialize("json",userobj) 
+            return user_api
+        else:
+            self.messagestack = []
+            self.messagestack.append({
+                "Message" : "This User is Blacklisted"
+            })
+            
         
         
     
