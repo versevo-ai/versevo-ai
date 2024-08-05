c24050f2c464b673294fe4168c2c1850acb61eaf
Sambit Chakraborty <smbtchakraborty@gmail.com>
2024-08-05 20:12:37 +0530

    Refactor: landing page

--- web/landing-e2e/playwright.config.ts
+++ web/landing-e2e/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx nx start my-next-app',
+    command: 'npx nx start web-landing',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
     cwd: workspaceRoot,
