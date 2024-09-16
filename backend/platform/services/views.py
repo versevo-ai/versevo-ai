b7abd59e376ba8663c80969c77e866f94347da97
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-09-16 18:51:25 +0530

    upd : logic of POST , PUT and PATCH in single function

--- backend/platform/services/views.py
+++ backend/platform/services/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.views import View
 from .serializers import *
 from .models import *
@@ -19,34 +20,32 @@ class serviceViews(LoginRequiredMixin, View):
             )
             return user_data
         else:
-            return {"Message": "Data Not Found"}
+            return JsonResponse({"Message": "Data Not Found"})
 
     def post(self, request):
         """
         Method to POST fresh Data to Server
         """
         serviceform_data = UserServiceForm(request.POST or None)
-        if serviceform_data.is_valid():
-            fetched_data = serviceform_data.cleaned_data
-            raw_user_data = ServiceModelSerializer(**fetched_data)
-            obj = raw_user_data.create_service_api()
-            return obj
-
-    def patch(self, request, username):
-        """
-        Method to UPDATE existing Data (Partial / Full)
-        """
-        updateuser_data = UserServiceForm(user=username, data=request.POST or None)  # type: ignore
-        if updateuser_data.is_valid():
-            fetched_data = updateuser_data.cleaned_data
-            raw_user_data = ServiceModelSerializer(**fetched_data)
-            obj = raw_user_data.update_service_api(username=username)
-            return obj
+        try:
+            if serviceform_data.is_valid():
+                fetched_data = serviceform_data.cleaned_data
+                raw_user_data = ServiceModelSerializer(**fetched_data)
+                if raw_user_data.method in ['POST','post']:
+                    obj = raw_user_data.create_service_api()
+                    return obj
+                elif raw_user_data.method in ['PUT','put','PATCH','patch']:
+                    obj = raw_user_data.update_service_api(username=raw_user_data.username)
+        except Exception:
+            return JsonResponse({"Status":"ERROR","Message":"Invalid Data in Form"})
 
     def delete(self, request, username):
         """
         Method to DELETE an user's data
         """
-        if username and services.objects.filter(username=username):
-            services.objects.filter(username=username).all().delete()
-            return {"Message": f"Data of {username} is deleted"}
+        try:
+            if username and services.objects.filter(username=username):
+                services.objects.filter(username=username).all().delete()
+                return {"Message": f"Data of {username} is deleted"}
+        except Exception:
+            return JsonResponse({"Message":"User Does not Exists"})
