17950f03161a2d9e2503275ea4635cb9ebb99407
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-09-16 18:54:08 +0530

    upd : logic for POST , PUT and PATCH in sngle function

--- backend/platform/users/views.py
+++ backend/platform/users/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.views import View
 from .serializers import *
 from .models import *
@@ -30,7 +27,7 @@ class userViews(LoginRequiredMixin, View):
             )
             return user_data
         else:
-            raise ValueError({"Message": "No User Exists"})
+            return JsonResponse({"Status":"ERROR","Message":f"username {username} already exists"})
 
     def post(self, request):
 
@@ -38,72 +35,53 @@ class userViews(LoginRequiredMixin, View):
         Method to POST fresh Data to Server
         """
         userform_data = UserForm(request.POST or None)
-        if userform_data.is_valid():
-            password = userform_data.clean_password2()
-            fetched_data = userform_data.cleaned_data
-            raw_user_data = UserModelSerializer(
-                username=fetched_data.get("username"),
-                email=fetched_data.get("email"),
-                password=password,
-                first_name=fetched_data.get("first_name"),
-                last_name=fetched_data.get("last_name"),
-            )
-            if raw_user_data.is_valid():
-                obj = raw_user_data.create_user_api()
-                user_obj = NewUser.objects.filter(
-                    username=fetched_data.get("username")
-                ).all()
-                login(request, user_obj) # type: ignore
-                return obj
-            else:
-                raise raw_user_data.messagestack # type: ignore
-
-    def patch(self, request):
-
-        """
-        Method to UPDATE existing Data (Partial / Full)
-        """
-        if request.user is not None:
-            updateuser_data = updateUserForm(
-                user=request.user, data=request.POST or None # type: ignore
-            )
-            if updateuser_data.is_valid():
-                fetched_data = updateuser_data.cleaned_data
-                raw_user_data = UserModelSerializer(**fetched_data)
-                if raw_user_data.is_valid():
-                    if (
-                        raw_user_data.password
-                        != NewUser.objects.get(username=request.user.username).password
-                    ):
-                        form = PasswordChangeForm(
-                            user=request.user, data=raw_user_data.password
-                        )
-                        if form.is_valid():
-                            form.save()
-                            update_session_auth_hash(request, form.user)
-                            obj = raw_user_data.update_user_api() # type: ignore
-                            return obj
-                        else:
-                            return {"Message": "Password is not Valid"}
-                    else:
-                        obj = raw_user_data.update_user_api() # type: ignore
+        try:
+            if userform_data.is_valid():
+                password = userform_data.clean_password2()
+                fetched_data = userform_data.cleaned_data
+                raw_user_data = UserModelSerializer(
+                    username=fetched_data.get("username"),
+                    email=fetched_data.get("email"),
+                    password=password,
+                    first_name=fetched_data.get("first_name"),
+                    last_name=fetched_data.get("last_name"),
+                )
+                if raw_user_data==True:
+                    if raw_user_data.method in ['POST','post']:
+                        obj = raw_user_data.create_user_api()
+                        user_obj = NewUser.objects.filter(username=fetched_data.get("username")).all()
+                        login(request, user_obj)
                         return obj
-            else:
-                raise raw_user_data.messagestack # type: ignore
-        else:
-            obj = {"Message": "No User Exists"}
-            raise ValueError(obj)
+                    elif raw_user_data.method in ['PUT','put','PATCH','patch']:
+                        if (raw_user_data.password != NewUser.objects.get(username=request.user.username).password):
+                            form = PasswordChangeForm(user=request.user, data=raw_user_data.password)
+                            try:
+                                if form.is_valid():
+                                    form.save()
+                                    update_session_auth_hash(request, form.user)
+                                    obj = raw_user_data.update_user_api() 
+                                    return obj
+                            except Exception:
+                                return JsonResponse({"Message": "Password is not Valid"})
+                        else:
+                            obj = raw_user_data.update_user_api(username=raw_user_data.username) 
+                            return obj
+                else:
+                    return raw_user_data.throw_errorlist()
+        except Exception:
+            return JsonResponse({"Status":"ERROR","Message":"Invalid Data in Form"})
+
 
     def delete(self, request):
         """
         Method to DELETE an user's data
         """
-        if request.user is not None:
-            uname = request.user.username
-            if NewUser.objects.get(username=uname).Blacklisted:
-                NewUser.objects.filter(username=uname).all().delete()
-                logout(request)
-                return {"Message": f"User Object of Username {uname} is deleted"}
-        else:
-            obj = {"Message": "No User Exists"}
-            raise ValueError(obj)
+        try:
+            if request.user is not None:
+                uname = request.user.username
+                if NewUser.objects.get(username=uname).Blacklisted:
+                    NewUser.objects.filter(username=uname).all().delete()
+                    logout(request)
+                    return {"Message": f"User Object of Username {uname} is deleted"}
+        except Exception:
+            return JsonResponse({"Status":"ERROR","Message":"User Does Not Exists"})
