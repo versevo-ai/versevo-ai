054bd832498e153a0c65e4c24b7cb327b7c353dc
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-09-16 18:52:04 +0530

    upd : whole logic with service model

--- backend/platform/tests/test_services.py
+++ backend/platform/tests/test_services.py
@@ -1,12 +1,12 @@
 from django.http import JsonResponse
 from django.test import Client
-from django.urls import reverse
 import pytest
 from services.models import *
 from services.serializers import *
 from services.forms import *
 
-class TestServices:
+@pytest.mark.django_db
+class TestUsers:
     @pytest.fixture(scope="function")
     def client(self):
         ''' 
@@ -14,127 +14,68 @@ class TestServices:
         '''
         return Client()
     
+    
     @pytest.fixture(scope="function")
     def get_test_database(self):
         '''
         A fixture method to get the mock or fake database
         '''
-        TestDatabase = {}
-        return TestDatabase
-    
+        return services.objects.all()
+
+        
     @pytest.fixture(scope="function")
-    def test_create_data(self,**data):
-        '''
-        A mock fixture method to serialize the raw data into a dict
-        '''
-        return {
-            'username' : data.get("username"),
-            'money' : data.get("money"),
-            'text_to_speech' : data.get("text_to_speech"),
-            'speech_to_text' : data.get("speech_to_text"),
-            'speech_to_speech' : data.get("speech_to_speech")
-        }
-    
-    @pytest.fixture(scope="function")
-    def test_create_api(self,**data):
+    def test_create_object(self,**data):
         '''
         A mock fixture method to serialize the dictionary data into an JSON based REST API
         '''
-        obj = serialize("json",self.test_create_data(data=data)) # type: ignore
-        self.get_test_database()[obj.username] = obj
-        return obj
+        serializer = ServiceModelSerializer(**data)
+        if serializer==[]:
+            res = serializer.create_service_api()
+            return res # May return errors caused in Creating Api
+        else:
+            return serializer # return errors caused while validating data
     
     @pytest.fixture(scope="function")
-    def test_update_api(self,username,**data):
+    def test_update_object(self,username,**data):
         '''
         A mock fixture method to update any data
         '''
-        new_obj =  self.test_create_data(data=data)
-        old_obj = self.get_test_database()[username]
-        # if new_obj['username'] != old_obj['username']:
-        #     old_obj['username'] = new_obj['username']
-        #     self.get_test_database()[username] = old_obj
-
-        if new_obj['money'] != old_obj['money']:
-            old_obj['money'] = new_obj['money']
-            
-        if new_obj['text_to_speech'] != old_obj['text_to_speech']:
-            old_obj['text_to_speech'] = new_obj['text_to_speech']
-            
-        if new_obj['speech_to_text'] != old_obj['speech_to_text']:
-            old_obj['speech_to_text'] = new_obj['speech_to_text']
-            
-        if new_obj['speech_to_speech'] != old_obj['speech_to_speech']:
-            old_obj['speech_to_speech'] = new_obj['speech_to_speech']
-        
-        return old_obj
+        serializer = ServiceModelSerializer(**data)
+        if serializer==[]:
+            res = serializer.update_service_api(username=username)
+            return res # May return errors caused in Updating Api
+        else:
+            return serializer # return errors caused while validating data
     
     def test_get_method(self):
         '''
         Testcase for applying GET request on the testdatabase
         '''
-        response = self.client().get(reverse('get_test_database'))
-        print(response.content)
-        assert response.status_code == 200
+        return self.get_test_database()
     
-    def test_post_method(self):
+    
+    def test_post_method(self,**data): 
         '''
         Testcase for applying POST request on the testdatabase
         '''
-        entrycount = len(self.get_test_database().keys())
-        data = send_data_to_testclass()
-        response = self.client().post(reverse("test_create_api"),data=data,format="json")
-        entrycount += 1
-        assert response.status_code == 201 and entrycount == len(self.get_test_database().keys())
+        return self.test_create_object(data=data)
     
-    def test_patch_method(self):
+    def test_patch_method(self,username,**data):
         '''
         Testcase for applying PATCH request on the testdatabase
         '''
-        db = self.get_test_database()
-        data = send_data_to_testclass()
-        username = data['username']
-        if username in db.keys():
-            oldcontent = db[username]
-            response = self.client().patch(reverse("test_update_api"),kwargs={"username":username},data=data,format="json") # type: ignore
-            assert response.status_code == 200 and oldcontent != response.content
+        return self.test_update_object(username=username,data=data)
     
-    def test_delete_method(self):
+    def test_delete_method(self,username):
         '''
         Testcase for applying DELETE request on the testdatabase
         '''
-        db = self.get_test_database()
-        entriescount = len(db.keys())
-        obj = send_data_to_testclass()
-        username = obj.get("username")
-        if username in db.keys():
-            del db[username]
-            del obj
-            entriescount -= 1
-            assert entriescount == len(db.keys())
+        services.objects.filter(username=username).all().delete()
+        return JsonResponse({"Message":f"Object of Username {username} deleted"})
     
     def teardown_method(self):
         del self
         return JsonResponse({
             "Message" : "Testsuit executed successfully"
-        })
-
-
-def send_data_to_testclass()->dict:
-    username , money , text_to_speech , speech_to_text , speech_to_speech = tuple(input().split(" "))
-    return {
-        "username":username or None,
-        "money":money or None,
-        "text_to_speech":text_to_speech or None,
-        "speech_to_text":speech_to_text or None,
-        "speech_to_speech":speech_to_speech or None
-    }
-      
+        })      
             
-                
-        
-        
-    
-        
-    
-    
