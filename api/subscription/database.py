783f7fcdd5125f907dff37bcedc36a768909c7c0
Sambit Chakraborty <smbtchakraborty@gmail.com>
2024-09-30 21:56:58 +0530

    Fix: Env value

--- api/subscription/database.py
+++ api/subscription/database.py
@@ -1,20 +1,22 @@
 import os
 from supabase import create_client, Client
 from subscription.schema.waitlist_schema import WaitlistSchema
+from tomlkit import table
 
 url: str = os.environ.get("SUPABASE_URL") # type: ignore
 key: str = os.environ.get("SUPABASE_KEY") # type: ignore
+table_name: str = os.environ.get("SUPABASE_TABLE_NAME") # type: ignore
 
 supabase: Client = create_client(url, key) # type: ignore
 
 def get_supabase():
-    response = supabase.table("subscribe_coming_soon").select("*").execute()
+    response = supabase.table(table_name).select("*").execute()
     return response
 
 def post_supabase(data: WaitlistSchema):
     response = (
         supabase
-        .table("subscribe_coming_soon")
+        .table(table_name)
         .insert({"email_id": data.email_id,
                  "created_at": data.created_at})
     )
@@ -22,7 +24,7 @@ def post_supabase(data: WaitlistSchema):
 
 def check_supabase(email: str) -> bool:
     response = (
-      supabase.table("subscribe_coming_soon")
+      supabase.table(table_name)
       .select("email_id")
       .eq("email_id", email)
       .execute()
