fcbc2c217d29ca0dc61cd4ecf8dbbcf1698ad0ff
Sambit Chakraborty <smbtchakraborty@gmail.com>
2024-10-22 20:14:28 +0530

    fix build dir

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  distDir: 'build',
+}
