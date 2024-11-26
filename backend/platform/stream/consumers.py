d03e6d88dd3774521cace24ab3fb54e57b0166ab
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-11-26 22:22:52 +0530

    add : basic websocket setup

---
+++ backend/platform/stream/consumers.py
@@ -0,0 +1,25 @@
+from channels.generic.websocket import JsonWebsocketConsumer
+
+class MyConsumer(JsonWebsocketConsumer):
+
+    def connect(self):
+        # Called on connection.
+        # To accept the connection call:
+        self.accept()
+        # Or accept the connection and specify a chosen subprotocol.
+        # A list of subprotocols specified by the connecting client
+        # will be available in self.scope['subprotocols']
+        self.accept("subprotocol")
+        # To reject the connection, call:
+        self.close()
+
+    def receive(self, text_data=None, bytes_data=None):
+        # Called with either text_data or bytes_data for each frame
+        # You can call:
+        self.send_json(content="Hello world! , sent")
+        self.receive_json(content="Hello world! , received")
+        self.close()
+
+    def disconnect(self, close_code):
+        # Called when the socket closes
+        pass
