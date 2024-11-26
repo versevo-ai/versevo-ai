4d7d26279fd7ddea1d0180a9d276106bf38d42fe
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-11-26 22:21:55 +0530

    upd : ASGI Config for Websocket

--- backend/platform/engine/asgi.py
+++ backend/platform/engine/asgi.py
@@ -8,9 +8,27 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
-from django.core.asgi import get_asgi_application
+import django
+from channels.routing import get_default_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "engine.settings")
+django.setup()
+application = get_default_application()
 
-application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
+
+    # WebSocket chat handler
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter([
+                path("chat/admin/", AdminChatConsumer.as_asgi()),
+                path("chat/", PublicChatConsumer.as_asgi()),
+            ])
+        )
+    ),
+})
