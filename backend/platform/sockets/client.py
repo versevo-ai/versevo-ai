b26d2029e57362cdb25970de6e55dcc979632f7b
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-12-06 20:14:43 +0530

    upd : SSL context

--- backend/platform/sockets/client.py
+++ backend/platform/sockets/client.py
@@ -1,11 +1,28 @@
+import ssl
+import pathlib
 from websockets.sync.client import connect
 
-def client(msg="Hello"):
-    URI = "wss://localhost:8765/"
-    try:
-        with connect(URI) as websocket:
-            websocket.send(msg)
-            response = websocket.recv()
-            print(f"Received from server: {response}")
-    except Exception as e:
-        print(f"Error: {e}")
+# Create SSL context for client
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+localhost_pem = pathlib.Path(__file__).with_name("localhost.pem")
+ssl_context.load_verify_locations(localhost_pem)
+
+URI = "wss://localhost:8765/dj/send"
+try:
+    with connect(uri=URI, ssl=ssl_context, server_hostname="localhost") as websocket:
+        websocket.send("Hii From Client at /dj/send")
+        response = websocket.recv()
+        print(f"server: {response}")
+except Exception as e:
+    print(f"Error: {e}")
+
+
+
+URI = "wss://localhost:8765/dj/rv"
+try:
+    with connect(uri=URI, ssl=ssl_context, server_hostname="localhost") as websocket:
+        websocket.send("Hii From Client at /dj/rv")
+        response = websocket.recv()
+        print(f"server: {response}")
+except Exception as e:
+    print(f"Error: {e}")
--- backend/platform/sockets/server.py
+++ backend/platform/sockets/server.py
@@ -1,21 +1,25 @@
-from websockets.sync.server import serve
+from websockets.sync.server import *
 import ssl
 import pathlib
-from client import client
+# from client import Client
 
 # Create SSL context
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 localhost_pem = pathlib.Path(__file__).with_name("localhost.pem")
 ssl_context.load_cert_chain(localhost_pem)
 
-def echo():
-    for messge in client():
-        print(messge)
+def handler(websocket:ServerConnection):
+    try:
+        stream = websocket.recv()
+        print(f"Client : {stream}")
+        websocket.send("Thank You ,from Server",text=True)
+    except Exception as e:
+        print(f"Error Occured : {e}")
 
 def main():
     try:
         # Start the server
-        with serve(echo, "localhost", 8765, ssl_context=ssl_context) as server:
+        with serve(handler=handler, host="localhost", port=8765, ssl_context=ssl_context) as server:
             print("WebSocket server running on wss://localhost:8765")
             server.serve_forever()
     except Exception as e:
