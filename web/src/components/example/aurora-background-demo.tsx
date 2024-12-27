8c169c624290fad5cd9444561cea54a5da661bbb
Sambit Chakraborty <smbtchakraborty@gmail.com>
2024-12-27 21:50:46 +0530

    Update: Coming soon component

--- web/src/components/example/aurora-background-demo.tsx
+++ web/src/components/example/aurora-background-demo.tsx
@@ -18,14 +18,11 @@ export default function AuroraBackgroundDemo() {
         className="relative flex flex-col gap-4 items-center justify-center px-4"
       >
         <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-          Background lights are cool you know.
+          coming soon
         </div>
         <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-          And this, is chemical burn.
+          VersevoAI
         </div>
-        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
-          Debug now
-        </button>
       </motion.div>
     </AuroraBackground>
   );
