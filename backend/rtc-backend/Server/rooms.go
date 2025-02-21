ae3dee1d9a056e443ecb76a67d412acf4d64d3f7
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2025-02-21 20:06:21 +0530

    Feat/versevo platform backend (#19)
    * initial refractor
    
    * Refactor : Projecta-App Architechture
    
    * add : init in api folder , secret key hardcoded
    
    * rm : secret key from project-settings
    
    * add : Routing Config For Multiple Databases
    
    * upd : Create & Update Api,Based On Payload
    
    * upd : Create & Update api function descriptions
    
    * rm : Seperate Config of test_user db
    
    * upd : logic - obj exists or not
    
    * upd : logic of POST , PUT and PATCH in single function
    
    * upd : whole logic with service model
    
    * upd : database_mark
    
    * upd : new_username field , returning value of check_pwd and more
    
    * rm : default test file for users
    
    * upd : logic for POST , PUT and PATCH in sngle function
    
    * rm : default test file for services
    
    * upd : cache config in settings
    
    * rm : nx files (partially)
    
    * add : demo cache file for services
    
    * add : App for Streaming Server
    
    * upd : python versions
    
    * upd : versions of all packages
    
    * add : stream app name
    
    * upd : settings update
    
    * add : channels and daphne
    
    * add : channel support for redis
    
    * upd : ASGI Config for Websocket
    
    * add : mention channel and daphne
    
    * add : basic websocket setup
    
    * rm : channels and channels-redis
    
    * rm : channels and daphne
    
    * add : websockets and msgpack packages
    
    * rm :  django app 'stream'
    
    * add : sockets directory
    
    * add : WS server config
    
    * add : SSL protection file
    
    * add : WS client config
    
    * add : demo client apis (divides in endpoints)
    
    * upd : SSL context
    
    * rm : scripts using websockets library and caches
    
    * add : .env of talk
    
    * add : project config
    
    * add : manage.py
    
    * add : client app
    
    * add : Go Webrtc Server Config
    
    * add : go webrtc server config
    
    * rm : django app talk
    
    * add : installations
    
    * upd : peerconnection by ICE
    
    * upd : handlePeerConnection
    
    * upd : PeerConnection
    
    * add : Code for handling rooms
    
    * add : Code for signalling server
    
    * rm : file
    
    * upd : dependencies
    
    * upd : handling connection
    
    * upd : ICE handler inside rooms and signal
    
    * upd : generate ICE from go-server

---
+++ backend/rtc-backend/Server/rooms.go
@@ -0,0 +1,96 @@
+package Server
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"github.com/pion/webrtc/v3"
+	"log"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type Participant struct {
+	Host           bool
+	Conn           *websocket.Conn
+	PeerConnection *webrtc.PeerConnection // Add PeerConnection
+}
+
+type RoomMap struct {
+	Mutex sync.RWMutex
+	Map   map[string][]Participant
+}
+
+func (r *RoomMap) Init() {
+	r.Map = make(map[string][]Participant)
+}
+
+func (r *RoomMap) Get(roomID string) []Participant {
+	r.Mutex.RLock()
+	defer r.Mutex.RUnlock()
+	return r.Map[roomID]
+}
+
+func (r *RoomMap) CreateRoom() string {
+	r.Mutex.Lock()
+	defer r.Mutex.Unlock()
+	rand.Seed(time.Now().UnixNano())
+	var Letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+	b := make([]rune, 8)
+
+	for i := range b {
+		b[i] = Letters[rand.Intn(len(Letters))]
+	}
+	roomID := string(b)
+	r.Map[roomID] = []Participant{}
+	return roomID
+}
+
+func (r *RoomMap) InsertIntoRoom(roomID string, host bool, conn *websocket.Conn) (*webrtc.PeerConnection, error) { // Return PeerConnection
+	r.Mutex.Lock()
+	defer r.Mutex.Unlock()
+
+	// Create Peer Connection
+	peerConnection, err := createPeerConnection()
+	if err != nil {
+		return nil, err // Handle error
+	}
+
+	p := Participant{host, conn, peerConnection} // Store PeerConnection
+	log.Println("Inserting into Room with RoomID: ", roomID)
+	r.Map[roomID] = append(r.Map[roomID], p)
+	return peerConnection, nil // Return the peer connection
+}
+
+func createPeerConnection() (*webrtc.PeerConnection, error) {
+
+	// Define ICE servers
+	iceServers := []webrtc.ICEServer{
+		{
+			URLs: []string{"stun:stun.l.google.com:19302"},
+		},
+	}
+
+	// Create a new RTCPeerConnection
+	config := webrtc.Configuration{
+		ICEServers: iceServers,
+	}
+
+	peerConnection, err := webrtc.NewPeerConnection(config)
+	if err != nil {
+		return nil, err
+	}
+
+	// Handle ICE connection state changes
+	peerConnection.OnICEConnectionStateChange(func(state webrtc.ICEConnectionState) {
+		fmt.Printf("ICE Connection State has changed: %s\n", state.String())
+	})
+
+	return peerConnection, nil
+}
+
+func (r *RoomMap) DeleteRoom(roomID string) {
+	r.Mutex.Lock()
+	defer r.Mutex.Unlock()
+	delete(r.Map, roomID)
+}
