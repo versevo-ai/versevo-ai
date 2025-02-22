b735e7d8156c6172463d53a7439319f27c708d04
PARTHIB DEB <parthibkumardeb@gmail.com>
2025-02-22 17:12:42 +0530

    upd : model structure

--- backend/platform/services/models.py
+++ backend/platform/services/models.py
@@ -5,16 +5,65 @@ from engine.settings import AUTH_USER_MODEL
 
 
 class services(models.Model):
-    username = models.CharField(null=True, blank=True, default="NULL")
-    money = models.FloatField(null=True, blank=True, default=0)
-    text_to_speech = models.BooleanField(null=True, blank=True, default=False)
-    speech_to_text = models.BooleanField(null=True, blank=True, default=False)
-    speech_to_speech = models.BooleanField(null=True, blank=True, default=False)
+    TTS_MODEL_NAMES= {
+        'TTS_Model1':'tts_model1',
+        'TTS_Model2':'tts_model2',
+        'TTS_Model3':'tts_model3',
+    }
+    
+    TTS_MODEL_PRICES= {
+        'tts_model1':100,
+        'tts_model2':200,
+        'tts_model3':300,
+    }
+    
+    
+    
+    STT_MODEL_NAMES= {
+        'STT_Model1':'stt_model1',
+        'STT_Model2':'stt_model2',
+        'STT_Model3':'stt_model3',
+    }
+    
+    STT_MODEL_PRICES= {
+        'stt_model1':100,
+        'stt_model2':200,
+        'stt_model3':300,
+    }
+    
+    
+    STS_MODEL_NAMES= {
+        'STS_Model1':'sts_model1',
+        'STS_Model2':'sts_model2',
+        'STS_Model3':'sts_model3',
+    }
+    
+    STS_MODEL_PRICES= {
+        'sts_model1':100,
+        'sts_model2':200,
+        'sts_model3':300,
+    }
+    
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE,related_name="NewUser")
+    money = models.IntegerField(null=True, blank=True, default=0)
+    tts_model = models.TextField(choices=TTS_MODEL_NAMES)
+    
+    stt_model = models.TextField(choices=STT_MODEL_NAMES)
+    
+    sts_model = models.TextField(choices=STS_MODEL_NAMES)
+    
+    def save(self, *args, **kwargs):
+        
+        price = self.TTS_MODEL_PRICES.get(self.TTS_MODEL_NAMES.get(self.tts_model))
+        print(price)
+        self.money += price
+        
+        price = self.STT_MODEL_PRICES.get(self.STT_MODEL_NAMES.get(self.stt_model))
+        self.money += price
+        
+        price = self.STS_MODEL_PRICES.get(self.STS_MODEL_NAMES.get(self.sts_model))
+        self.money += price
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.username} - TTS : {self.text_to_speech} , STT : {self.speech_to_text} , STS : {self.speech_to_speech}"
-
-
-class UserService(models.Model):
-    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
-    service = models.ForeignKey(services, on_delete=models.CASCADE)
+        return f"{self.user} spent {self.money}"
