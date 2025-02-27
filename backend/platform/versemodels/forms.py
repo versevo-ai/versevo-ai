b31ffedd8efacf8c8f805eda9a669d0555274ab8
PARTHIB DEB <parthibkumardeb@gmail.com>
2025-02-27 21:08:29 +0530

    rm : ChatModels Form

--- backend/platform/versemodels/forms.py
+++ backend/platform/versemodels/forms.py
@@ -1,12 +1,7 @@
 from django import forms
 from .models import *
 
-class ChatModelsForm(forms.ModelForm):
-    class Meta:
-        model = ChatModels
-        fields = "__all__"
-
 class ModelRequestResponseForm(forms.ModelForm):
     class Meta:
         model = ModelRequestResponse
-        fields = ["question","answer"]
+        fields = ["page_no","chat_no","question","answer"]
