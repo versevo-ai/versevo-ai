d9cbe83e9744f90983b73a1265a571cca2fb416a
Avik-creator <avikm744@gmail.com>
2025-06-01 17:39:00 +0530

    Added Changes.

--- web/src/components/ui/joinWaitlist.tsx
+++ web/src/components/ui/joinWaitlist.tsx
@@ -5,14 +5,14 @@ export default function JoinWaitlist() {
                 <input
                     type="email"
                     placeholder="Enter your Email Address"
-                    className="flex-1 px-6 py-3 text-lg text-[#075985] bg-[#DFF6FD] focus:outline-none rounded-full"
+                    className="flex-1 px-2 py-2 text-lg text-[#075985] bg-[#DFF6FD] focus:outline-none rounded-full"
                     required
                 />
                 
-                <div className="p-[2px] rounded-full bg-[conic-gradient(at_top_right,_#0D72DF,_#073E79,_#88E4FF,_#073E79)]">
+                <div className="p-[2px] rounded-full bg-[conic-gradient(at_center,_#0D72DF,_#073E79,_#88E4FF,_#073E79)]">
                     <button
                         type="submit"
-                        className="px-6 py-3 text-lg font-semibold text-[#075985] bg-[#B2E2F0] rounded-full hover:bg-[#cbe9f6] transition-colors w-full h-full"
+                        className="text-lg px-2 py-2 font-semibold text-[#075985] bg-[#B2E2F0] rounded-full hover:bg-[#cbe9f6] transition-colors w-full h-full"
                     >
                         Join Waitlist
                     </button>
